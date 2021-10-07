1658d34f1567d94b93ba2f9a7920518a1dd47ab5
voncardoso <vonharrison26@gmail.com>
2021-10-07 17:19:43 -0300

    rota de login para contac criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Contact } from './components/Contatc';
-
+import { Login } from './components/Login';
+import { BrowserRouter, Route , Switch } from "react-router-dom";
 
 export function App() {
   return ( 
-    <Contact/>
+    <BrowserRouter>
+
+    <Switch>
+      <Route path="/" exact component={Login}/>
+      <Route path="/contact" component={Contact}/>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+export function Login(){
+    return(
+       <a href="/contact">
+           Castrar
+       </a>
+    )
+   }

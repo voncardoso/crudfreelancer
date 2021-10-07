ff50e8b0fdaf531e100f6462795477d32b8bd152
voncardoso <vonharrison26@gmail.com>
2021-10-07 13:37:53 -0300

    mostrar imagem na tela

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,22 +1,47 @@
-import { FormEvent, useState } from "react";
+import { database } from "firebase";
+import { Children, FormEvent, useEffect, useRef, useState } from "react";
 import firebaseDb from "../firebase";
+import './styles.css';
 
+
+
+interface Idtype{
+    email:string,
+    passaword: string,
+
+}
 
 
 export function ContactForm(){
-    var [email, SetEmail] = useState('');
-    var [passaword, setPassaword] = useState('');
-
-       function handleFormSubmit(event: FormEvent){
+    const [email, SetEmail] = useState('');
+    const [passaword, setPassaword] = useState('');
+    const [contactObjects, setContactObjects] = useState<Idtype[]>([]);
+   
+    function handleFormSubmit(event: FormEvent){
         event.preventDefault();
-        const data = {
+        const data= {
             email,
             passaword,
         }
 
         firebaseDb.child('contacts').push(data);
        }
+
+       useEffect(() => {
+        firebaseDb.child('contacts').on('value', snapshot => {
+            if (snapshot?.val() != null)
+                setContactObjects({
+                    ...snapshot.val()
+                })
+            else
+                setContactObjects([])
+
+        })
+    }, [])// similar to componentDidMount
+
+
     return(
+        <>
         <form onSubmit={handleFormSubmit}>
             <input 
                 placeholder="Email" 
@@ -31,6 +56,26 @@ export function ContactForm(){
             <button type="submit">
                 Cadatrar
             </button>
+            <thead >
+                <tr>
+                    <th>Email</th>
+                    <th>Password</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    Object.keys(contactObjects).map((id: any) => {
+                        return (
+                            <tr key={id}>
+                            <td>{contactObjects[id].email}</td>
+                            <td>{contactObjects[id].passaword}</td>
+                        </tr>
+                        )
+                    })
+                }
+            </tbody>
         </form>
+        </>
     );
 }
